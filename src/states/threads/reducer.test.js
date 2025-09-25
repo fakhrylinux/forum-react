2dfe9b68e1467dc86c1fecf94615dfd7f392fc11
Fakhry Linux <fakhry.linux@gmail.com>
2025-09-25 20:51:08 +0800

    Add unit test

---
+++ src/states/threads/reducer.test.js
@@ -0,0 +1,203 @@
+import { describe, it, expect } from 'vitest';
+import threadsReducer from './reducer.js';
+
+describe('threadsReducer', () => {
+  it('should return the initial state when given by unknown action', () => {
+    // arrange
+    const initialState = [];
+    const action = { type: 'UNKNOWN' };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual(initialState);
+  });
+
+  it('should return the threads when given by RECEIVE_THREADS action', () => {
+    // arrange
+    const initialState = [];
+    const action = {
+      type: 'RECEIVE_THREADS',
+      payload: {
+        threads: [
+          {
+            'id': 'thread-1',
+            'title': 'Thread Pertama',
+            'body': 'Ini adalah thread pertama',
+            'category': 'General',
+            'createdAt': '2021-06-21T07:00:00.000Z',
+            'ownerId': 'users-1',
+            'upVotesBy': [],
+            'downVotesBy': [],
+            'totalComments': 0
+          },
+          {
+            'id': 'thread-2',
+            'title': 'Thread Kedua',
+            'body': 'Ini adalah thread kedua',
+            'category': 'General',
+            'createdAt': '2021-06-21T07:00:00.000Z',
+            'ownerId': 'users-2',
+            'upVotesBy': [],
+            'downVotesBy': [],
+            'totalComments': 0
+          }
+        ]
+      }
+    };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual(action.payload.threads);
+  });
+
+  it('should return the threads with the new thread when given by ADD_THREAD action', () => {
+    // arrange
+    const initialState = [
+      {
+        'id': 'thread-1',
+        'title': 'Thread Pertama',
+        'body': 'Ini adalah thread pertama',
+        'category': 'General',
+        'createdAt': '2021-06-21T07:00:00.000Z',
+        'ownerId': 'users-1',
+        'upVotesBy': [],
+        'downVotesBy': [],
+        'totalComments': 0
+      }];
+
+    const action = {
+      type: 'ADD_THREAD',
+      payload: {
+        thread:{
+          'id': 'thread-2',
+          'title': 'Thread Kedua',
+          'body': 'Ini adalah thread kedua',
+          'category': 'General',
+          'createdAt': '2021-06-21T07:00:00.000Z',
+          'ownerId': 'users-2',
+          'upVotesBy': [],
+          'downVotesBy': [],
+          'totalComments': 0
+        }
+      }
+    };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual([action.payload.thread, ...initialState]);
+  });
+
+  it('should return the threads with upvoted thread when given by UPVOTE_THREAD action', () => {
+    // arrange
+    const initialState = [
+      {
+        'id': 'thread-1',
+        'title': 'Thread Pertama',
+        'body': 'Ini adalah thread pertama',
+        'category': 'General',
+        'createdAt': '2021-06-21T07:00:00.000Z',
+        'ownerId': 'users-1',
+        'upVotesBy': [],
+        'downVotesBy': [],
+        'totalComments': 0
+      }
+    ];
+
+    const action = {
+      type: 'UPVOTE_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'users-1'
+      }
+    };
+
+    // action: upvote thread
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual([
+      {
+        ...initialState[0],
+        upVotesBy: [action.payload.userId]
+      }
+    ]);
+  });
+
+  it('should return the threads with downvoted thread when given by DOWNVOTE_THREAD action', () => {
+    // arrange
+    const initialState = [
+      {
+        'id': 'thread-1',
+        'title': 'Thread Pertama',
+        'body': 'Ini adalah thread pertama',
+        'category': 'General',
+        'createdAt': '2021-06-21T07:00:00.000Z',
+        'ownerId': 'users-1',
+        'upVotesBy': [],
+        'downVotesBy': [],
+        'totalComments': 0
+      }
+    ];
+
+    const action = {
+      type: 'DOWNVOTE_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'users-1'
+      }
+    };
+
+    // action: upvote thread
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual([
+      {
+        ...initialState[0],
+        downVotesBy: [action.payload.userId]
+      }
+    ]);
+  });
+
+  it('should return the threads with neutralize thread when given by NEUTRALIZE_THREAD action', () => {
+    // arrange
+    const initialState = [
+      {
+        'id': 'thread-1',
+        'title': 'Thread Pertama',
+        'body': 'Ini adalah thread pertama',
+        'category': 'General',
+        'createdAt': '2021-06-21T07:00:00.000Z',
+        'ownerId': 'users-1',
+        'upVotesBy': ['users-1'],
+        'downVotesBy': [],
+        'totalComments': 0
+      }
+    ];
+
+    const action = {
+      type: 'NEUTRALIZE_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'users-1'
+      }
+    };
+
+    // action: upvote thread
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual([
+      {
+        ...initialState[0],
+        upVotesBy: []
+      }
+    ]);
+  });
+});
---
+++ src/states/users/action.test.js
@@ -0,0 +1,73 @@
+import { describe, beforeEach, afterEach, it, vi, expect } from 'vitest';
+import api from '../../utils/api.js';
+import { asyncRegisterUser } from './action.js';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+
+const fakeRegisterUserRequest= {
+  name: 'John Doe',
+  email: 'john@example.com',
+  password: 'password'
+};
+
+const fakeRegisterUserResponse = [
+  {
+    'id': 'user-123',
+    'name': 'John Doe',
+    'email': 'john@example.com',
+    'avatar': 'https://generated-image-url.jpg'
+  }
+];
+
+const fakeErrorResponse = new Error('Ups, something went wrong');
+
+describe('asyncRegisterUser thunk', ()=> {
+  beforeEach(()=> {
+    api._register = api.register;
+  });
+
+  afterEach(()=> {
+    api.register = api._register;
+
+    delete api._register;
+  });
+
+  it('should dispatch action correctly when register user success', async () => {
+    //  arrange
+
+    // mock register implementation
+    api.register = vi.fn().mockImplementation(()=> Promise.resolve(fakeRegisterUserResponse));
+
+    // mock dispatch
+    const dispatch = vi.fn();
+
+    // action
+    await asyncRegisterUser(fakeRegisterUserRequest)(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(api.register).toHaveBeenCalledWith(fakeRegisterUserRequest);
+  });
+
+  it('should dispatch action and call alert correctly when register user failed', async () => {
+    //  arrange
+
+    // mock register implementation
+    api.register = vi.fn().mockImplementation(()=> Promise.reject(fakeErrorResponse));
+
+    // mock dispatch
+    const dispatch = vi.fn();
+
+    // mock alert
+    window.alert = vi.fn();
+
+    // action
+    await asyncRegisterUser(fakeRegisterUserRequest)(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(api.register).toHaveBeenCalledWith(fakeRegisterUserRequest);
+    expect(window.alert).toHaveBeenCalledWith(fakeErrorResponse.message);
+  });
+});
