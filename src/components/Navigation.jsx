9c87f0c31a326cc5c57da988020d11808c33e30b
Fakhry Linux <fakhry.linux@gmail.com>
2025-09-26 22:39:15 +0800

    Change back logo title using h5

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,7 @@ function Navigation({ authUser, signOut }) {
     <header className="header">
       <div className="header-logo">
         <NavLink to="/">
-          <h2>Temu Wicara</h2>
+          <h5>Temu Wicara</h5>
         </NavLink>
       </div>
       <div className="header-icons">
